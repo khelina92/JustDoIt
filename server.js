38ade50e581b03b75e64dfd6637c08ba89a0c9dc
Julie Bringsrud <juliem15@uia.no>
2017-11-08 17:37:36 +0100

    dbstring

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const url = require('url');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const {Client} = require("pg");
 
+let dbString = ""
 
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static('public'));
@@ -13,6 +15,25 @@ app.listen(app.get('port'), function() {
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+app.get('/lists',function(req,res){
+   
+       let client = new Client({
+        connectionString:process.env.DATABASE_URL || dbString,
+        ssl:true
+    });
+
+    client.connect();
+
+    client.query("select * from lists", (err,resp) =>{
+        
+       
+        res.json(resp.rows).end();
+        client.end();
+    });
+
+    
+});
 
 
 
