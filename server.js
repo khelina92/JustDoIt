3ddd233539ed97fab8a0a4b2a9bd7279fbead269
Julie Bringsrud <juliem15@uia.no>
2017-11-14 14:46:59 +0100

    Rest API for user?

--- server.js
+++ server.js
@@ -25,6 +25,21 @@ app.get('/', function (req, res) {
 
 let dbString = "postgres://qiypkwjnjgwadw:ddf8a7f06464234473af6e17bd765d589a1ec038db7b514abdee5cf720293646@ec2-54-75-225-143.eu-west-1.compute.amazonaws.com:5432/da5jtj9gun137e"
 
+app.get('/users', function(req,res){
+    let client = new Client({
+    connectionString:process.env.DATABASE_URL || dbString,
+        ssl:true
+    });
+
+    client.connect();
+
+    client.query("select * from users", (err,resp) =>{
+        
+       
+        res.json(resp.rows).end();
+        client.end();
+    });
+});
 
 app.get('/lists',function(req,res){
    
