0a9f2e06809e2ebc85953cd84f75a5f20d2dc50d
Julie Bringsrud <juliem15@uia.no>
2017-11-14 13:23:45 +0100

    Pushet til Julie branch

--- server.js
+++ server.js
@@ -1,21 +1,50 @@
 const url = require('url');
 const express = require('express');
+const app = express(); //server-app
 const bodyParser = require('body-parser');
-const app = express();
+const {Client} = require("pg");
 
-
-app.set('port', (process.env.PORT || 8080));
+app.set('port', (process.env.PORT || 3000));
 app.use(express.static('public'));
-app.listen(app.get('port'), function() {
-    console.log("Super group server is running", app.get("port"));
-});
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+    
+  //set headers
+  res.set('Access-Control-Allow-Origin', '*'); 
+  res.set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    
+  //set statusline and body - and send
+  res.status(200).send('Hei verden!'); //status-line and body
+
+    let staticApp = readTextFile("index.html");
+    res.send(staticApp);
+    
+});
 
 
+let dbString = "postgres://qiypkwjnjgwadw:ddf8a7f06464234473af6e17bd765d589a1ec038db7b514abdee5cf720293646@ec2-54-75-225-143.eu-west-1.compute.amazonaws.com:5432/da5jtj9gun137e"
 
 
+app.get('/lists',function(req,res){
+   
+       let client = new Client({
+        connectionString:process.env.DATABASE_URL || dbString,
+        ssl:true
+    });
 
-//app.get()
+    client.connect();
+
+    client.query("select * from lists", (err,resp) =>{
+        
+       
+        res.json(resp.rows).end();
+        client.end();
+    });
+
+    
+});
+
+app.listen(3000, function () {
+  console.log('Server listening on port 3000!!!!');
+});
