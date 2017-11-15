1285b2bf9ab867702f10c0f1b80062602b606cfb
Julie Bringsrud <juliem15@uia.no>
2017-11-15 15:22:40 +0100

    Superfetch2,simpleclient,showlists,list.js

---
+++ list.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var db = require('./dbconnect');
+
+//endpoint: GET travels
+router.get('/', function (req,res){
+    
+    var sql = 'SELECT * FROM lists';
+    db.any(sql).then(function(data){
+                     
+        res.status(200).json(data); //success - send the data as JSON!
+                     }
+}).catch(function(err){
+         
+         res.status(500).json(err);
+         
+});
+
+});
+
+// export module
+module.exports = router;
