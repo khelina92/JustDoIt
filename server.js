d4d747810577eff5bd1d0f79f38321b467b48f90
khelina92 <30373346+khelina92@users.noreply.github.com>
2017-11-28 16:37:54 +0100

    Har kontakt med server Tirsdag 16.37
    Vi kan logge inn, lage lister på bruker, hente opp listene til brukeren

--- server.js
+++ server.js
@@ -83,7 +83,6 @@ app.post('/users/', bodyParser, function (req, res){
     let client = new Client({
             connectionString:process.env.DATABASE_URL || dbString,
             ssl:true
-
     });
 
     client.connect();
@@ -95,117 +94,117 @@ app.post('/users/', bodyParser, function (req, res){
         var tok = jwt.sign(payload, secret, {expiresIn: "12h"});
 
         //send logininfo + token to the client
-        res.status(200).json({username: upload.username, firstname: upload.firstname, lastname: upload.lastname, token: tok});
+        res.status(200).json({username: upload.username, firstname: upload.firstname, lastname: upload.lastname, token: tok}).end();
 
-        res.json({msg: "insert ok"}).end();
+        //res.json({msg: "insert ok"}).end();
         client.end();
     });
-
 });
 
 //-- LOG IN
 
    app.post('/users/auth/', bodyParser, function (req, res){
        
-       //res.set('Access-Control-Allow-Origin', '*');
-       //res.set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-       
-
-
         let upload = JSON.parse(req.body);
+       
        
         console.log(upload);
        
-       
         let encrPassw =  sha256(upload.password); //hasher passordet
        
-    
         let sql = `SELECT * FROM users WHERE username='${upload.username}'`;
        
        console.log(sql);
-       
         
         let client = new Client({
             connectionString:process.env.DATABASE_URL || dbString,
             ssl:true
         });
        
-
-
         client.connect();
 
         client.query(sql, (err,dbresp) =>{
 
             let dbpassw = dbresp.rows[0].password;
             
-
             if (dbresp.rows.length <= 0) {
                 dbresp.json({msg: "Vennligst skriv inn brukernavn"}).end();
             }
 
             else {
-
-
+                
                 if (dbpassw && encrPassw == dbpassw ){
 
                     //create token
                     let payload = {username: upload.username, firstname: upload.firstname, lastname: upload.lastname};
                     let tok = jwt.sign(payload, secret, {expiresIn: "12h"});
+                    
 
                     //send logininfo + token to the client
                     res.status(200).json({username: upload.username, firstname: upload.firstname, lastname: upload.lastname, token: tok, msg:"du er logget inn"}).end();
-                    
-                    
-
                 }
                 else {
                     res.status(401).json({msg: "Feil brukernavn og passord"}).end();
                 }
-
-                
-                //res.json({msg: "Du får logge inn"}).end();
-              
             };
        });
-       
-       
-        
-
    });
 
 
 
 //----GET LISTS
-    app.get('/lists/', function(req,res){
+
+    app.get('/lists/', bodyParser, function(req,res){
+
+        
+    
+        
+   // var upload = req.headers.cookie;
+     //   console.log(upload);
+   
+        
+    let token = req.query.token;
+    
+        
+    let logindata = jwt.verify(token, secret); //check the token
+        
+    //let sql = `PREPARE get_lists (text) AS SELECT * FROM lists WHERE username=$1; EXECUTE get_lists ('${logindata.username}')`;
+        
+     
+        
+        
+        
+    let sql = `SELECT * FROM lists WHERE username='${logindata.username}'`;
 
         let client = new Client({
-        connectionString:process.env.DATABASE_URL || dbString,
-        ssl:true
-    });
+                connectionString:process.env.DATABASE_URL || dbString,
+                ssl:true
+        });
+       
+        client.connect();
 
-    client.connect();
+        client.query(sql, (err,dbresp) =>{
 
-    client.query("select * from lists", (err,dbresp) =>{
-
-        dbresp.json(dbresp.rows).end();
-        client.end();
-    });
-
+           
+            res.status(200).json(dbresp.rows);
+            
+            
+       });
 
 });
 
 //------POST LISTE
 app.post('/lists/', bodyParser, function (req, res){
-
-    console.log("innie post list");
-
-
+    
     var upload = JSON.parse(req.body);
+    let token = req.query.token;
+   
+    
+    let logindata = jwt.verify(token, secret); //check the token
+    console.log(logindata);
+    var sql = `PREPARE insert_lists (int, text, text, timestamp, date, time, boolean, text) AS INSERT INTO lists VALUES(DEFAULT, $2, $3, $4, $5, $6, $7, $8); EXECUTE insert_lists (0, '${upload.listname}', '${upload.listdescription}', '2017-11-22 12:53','${upload.duedate}', '${upload.duetime}', 'true', '${logindata.username}')`;
 
-    var sql = `PREPARE insert_lists (int, text, text, timestamp, date, time, boolean, int) AS INSERT INTO lists VALUES(DEFAULT, $2, $3, $4, $5, $6, $7, $8); EXECUTE insert_lists (0, '${upload.listname}', '${upload.listdescription}', '2017-11-22 12:53','${upload.duedate}', '${upload.duetime}', 'true', 0)`;
-
-    console.log(sql)
-
+    
     let client = new Client({
             connectionString:process.env.DATABASE_URL || dbString,
             ssl:true
@@ -213,11 +212,9 @@ app.post('/lists/', bodyParser, function (req, res){
 
     client.connect();
 
-    client.query(sql, (err,dbresp) =>{
+    client.query(sql, (err, resp) =>{
 
-        console.log("inne i qutr");
-
-        dbresp.json(err).end();
+        res.json(err).end();
         client.end();
     });
 
@@ -225,10 +222,9 @@ app.post('/lists/', bodyParser, function (req, res){
 
 //----GET ITEMS
 
-    app.get('/listitems/',function(req,res){
-
+app.get('/listitems/',function(req,res){
     
-
+       
        let client = new Client({
         connectionString:process.env.DATABASE_URL || dbString,
         ssl:true
@@ -236,15 +232,46 @@ app.post('/lists/', bodyParser, function (req, res){
 
     client.connect();
 
-    client.query("select * from listitems", (err,res) =>{
+    client.query("select * from listitems", (err,resp) =>{
 
-        res.json(res.rows).end();
+        res.json(resp.rows).end();
         client.end();
     });
 
 });
 
+//----DELETE LISTS
+
+app.delete('/lists/', function (req,res){
+    
+    var upload = req.query.listid;
+    
+   
+    var sql = `PREPARE delete_list (int) AS DELETE FROM lists WHERE listid=$1 RETURNING *; EXECUTE delete_list('${upload}')`;
+    
+    let client = new Client({
+            connectionString:process.env.DATABASE_URL || dbString,
+            ssl:true
+    });
+
+    client.connect();
+
+    client.query(sql, (err,resp) =>{
+
+        //res.json({msg: "delete ok"}).end();
+        if (!err) {
+            res.status(200).json({msg: "delete ok"});
+        }
+        else{
+            res.status(200).json({msg: "can't delete"});
+        }
+        
+        client.end();
+
+    });      
+});
+
+
     app.listen(3000, function () {
         console.log('Server listening on port 3000!!!!');
     });
-
